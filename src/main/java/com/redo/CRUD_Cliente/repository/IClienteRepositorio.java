7507b9e94e3863c65d357775df81232f30de676c
abel-02 <abelaquino02j@gmail.com>
2024-08-06 19:46:55 -0300

    Correccion parametros incorrectos

--- src/main/java/com/redo/CRUD_Cliente/repository/IClienteRepositorio.java
+++ src/main/java/com/redo/CRUD_Cliente/repository/IClienteRepositorio.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IClienteRepositorio extends JpaRepository<Long, Cliente> {
+public interface IClienteRepositorio extends JpaRepository<Cliente, Long> {
 }
