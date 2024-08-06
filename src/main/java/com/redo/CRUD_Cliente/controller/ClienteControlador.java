1bf4f29d135ad91929f51a08548d92226f11300d
abel-02 <abelaquino02j@gmail.com>
2024-08-06 19:32:36 -0300

    Metodos principales del controlador

--- src/main/java/com/redo/CRUD_Cliente/controller/ClienteControlador.java
+++ src/main/java/com/redo/CRUD_Cliente/controller/ClienteControlador.java
@@ -1,24 +1,30 @@
 package com.redo.CRUD_Cliente.controller;
 
 import com.redo.CRUD_Cliente.model.Cliente;
+import com.redo.CRUD_Cliente.service.ClienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ClienteControlador {
     @Autowired
     private ClienteServicio servicio;
 
-    public void registrarCliente(Cliente cliente){
-
+    @PostMapping("/cliente")
+    public void registrarCliente(@RequestBody Cliente cliente){
+        servicio.guardarCliente(cliente);
     }
-    public void solicitarCliente(Long id){
-
+    @GetMapping("/cliente/{id}")
+    public void solicitarCliente(@PathVariable Long id){
+        servicio.obtenerCliente(id);
     }
-    public void eliminarCliente(Long id){
-
+    @DeleteMapping("/cliente/{id}")
+    public void eliminarCliente(@PathVariable Long id){
+        servicio.eliminarCliente(id);
     }
-    public void modificarCliente(Cliente cliente){
-
+    @PutMapping("/cliente")
+    public void modificarCliente(@RequestBody Cliente cliente){
+        servicio.modificarCliente(cliente);
     }
 }
