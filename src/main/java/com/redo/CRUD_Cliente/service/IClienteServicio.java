d6e31fdb1f0b8e8c8c6e7607ac55ed62c7456e17
abel-02 <abelaquino02j@gmail.com>
2024-08-06 19:59:17 -0300

    Correcion metodo obtenerCliente

--- src/main/java/com/redo/CRUD_Cliente/service/IClienteServicio.java
+++ src/main/java/com/redo/CRUD_Cliente/service/IClienteServicio.java
@@ -6,6 +6,6 @@ public interface IClienteServicio {
     public void guardarCliente(Cliente cliente);
     public void eliminarCliente(Long id);
     public void modificarCliente(Cliente cliente);
-    public void obtenerCliente(Long id);
+    public Cliente obtenerCliente(Long id);
 
 }
