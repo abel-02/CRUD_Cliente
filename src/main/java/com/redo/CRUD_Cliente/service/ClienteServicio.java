f320a1e25d780e0f0bcbbeb0479c81a17ef53658
abel-02 <abelaquino02j@gmail.com>
2024-08-06 19:42:35 -0300

    Metodos principales del servicio

--- src/main/java/com/redo/CRUD_Cliente/service/ClienteServicio.java
+++ src/main/java/com/redo/CRUD_Cliente/service/ClienteServicio.java
@@ -4,28 +4,36 @@ import com.redo.CRUD_Cliente.model.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ClienteServicio implements IClienteServicio{
     @Autowired
-    private ClienteRepositorio repositorio;
-    
+    private IClienteServicio repositorio;
+
     @Override
     public void guardarCliente(Cliente cliente) {
-
+        repositorio.guardarCliente(cliente);
     }
-
     @Override
     public void eliminarCliente(Long id) {
-
+        repositorio.eliminarCliente(id);
     }
-
     @Override
     public void modificarCliente(Cliente cliente) {
-
+        if(existeCliente(cliente.getId()))
+            repositorio.guardarCliente(cliente);
     }
-
     @Override
-    public void obtenerCliente(Long id) {
-
+    public Cliente obtenerCliente(Long id) {
+       if(existeCliente(id))
+           return repositorio.obtenerCliente(id);
+        return null;
+    }
+    private boolean existeCliente(Long id){
+        Cliente cliente = this.repositorio.obtenerCliente(id);
+        if(!(cliente == null))
+            return true;
+        return false;
     }
 }
