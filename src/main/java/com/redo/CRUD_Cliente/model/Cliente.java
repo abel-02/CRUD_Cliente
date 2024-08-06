a3fe986b8e2630071f40d09183d92406669d7c4c
abel-02 <abelaquino02j@gmail.com>
2024-08-06 20:13:09 -0300

    Anotaciones de la entidad para la base de datos

--- src/main/java/com/redo/CRUD_Cliente/model/Cliente.java
+++ src/main/java/com/redo/CRUD_Cliente/model/Cliente.java
@@ -1,12 +1,15 @@
 package com.redo.CRUD_Cliente.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity @Data @NoArgsConstructor@AllArgsConstructor
+@Table(name = "clientes")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String apellido;
