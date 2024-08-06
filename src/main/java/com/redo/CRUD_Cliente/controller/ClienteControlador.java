949d880ba5f2fb797bc18986696fa0a2f4250aba
abel-02 <abelaquino02j@gmail.com>
2024-08-06 19:19:15 -0300

    Creacion paquete controlador

---
+++ src/main/java/com/redo/CRUD_Cliente/controller/ClienteControlador.java
@@ -0,0 +1,24 @@
+package com.redo.CRUD_Cliente.controller;
+
+import com.redo.CRUD_Cliente.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ClienteControlador {
+    @Autowired
+    private ClienteServicio servicio;
+
+    public void registrarCliente(Cliente cliente){
+
+    }
+    public void solicitarCliente(Long id){
+
+    }
+    public void eliminarCliente(Long id){
+
+    }
+    public void modificarCliente(Cliente cliente){
+
+    }
+}
