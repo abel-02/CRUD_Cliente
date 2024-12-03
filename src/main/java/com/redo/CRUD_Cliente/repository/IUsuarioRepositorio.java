c2e95e24e50b5120d4e163636ae24d77ee8702a6
abel-02 <abelaquino02j@gmail.com>
2024-12-03 20:56:11 -0300

    Método buscarPorEmailYContra en repositorio de usuario

--- src/main/java/com/redo/CRUD_Cliente/repository/IUsuarioRepositorio.java
+++ src/main/java/com/redo/CRUD_Cliente/repository/IUsuarioRepositorio.java
@@ -2,8 +2,16 @@ package com.redo.CRUD_Cliente.repository;
 
 import com.redo.CRUD_Cliente.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IUsuarioRepositorio extends JpaRepository<Usuario, Long> {
+    @Query("select c from Usuario c where c.email = :email AND c.password = :password")
+    List<Usuario> buscarPorEmailYContra(@Param("email") String email,
+                                        @Param("password") String password);
+
 }
