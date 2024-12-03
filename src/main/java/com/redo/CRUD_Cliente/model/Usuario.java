fe8575fbf031b692306653b08130fc7fa3a55066
abel-02 <abelaquino02j@gmail.com>
2024-12-03 20:13:20 -0300

    Agrega contraseña a un usuario

--- src/main/java/com/redo/CRUD_Cliente/model/Usuario.java
+++ src/main/java/com/redo/CRUD_Cliente/model/Usuario.java
@@ -1,5 +1,6 @@
 package com.redo.CRUD_Cliente.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,5 +19,7 @@ public class Usuario  {
     private String apellido;
     private String email;
     private String direccion;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 
 }
