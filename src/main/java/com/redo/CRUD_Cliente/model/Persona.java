ddb80db909b1bd3bcdf856e0d0d20389af2b6e9e
abel-02 <abelaquino02j@gmail.com>
2024-12-03 11:36:08 -0300

    Crea entidad persona

---
+++ src/main/java/com/redo/CRUD_Cliente/model/Persona.java
@@ -0,0 +1,22 @@
+package com.redo.CRUD_Cliente.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String direccion;
+}
