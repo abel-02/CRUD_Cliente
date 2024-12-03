81300519d97d16834770e21a65648b3e68b333c2
abel-02 <abelaquino02j@gmail.com>
2024-12-03 11:43:41 -0300

    Entidad cliente hereda de persona

--- src/main/java/com/redo/CRUD_Cliente/model/Cliente.java
+++ src/main/java/com/redo/CRUD_Cliente/model/Cliente.java
@@ -3,16 +3,11 @@ package com.redo.CRUD_Cliente.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity @Data @NoArgsConstructor@AllArgsConstructor
 @Table(name = "clientes")
-public class Cliente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nombre;
-    private String apellido;
-    private String email;
-    private String direccion;
+@EqualsAndHashCode(callSuper = true)
+public class Cliente extends Persona{
 }
